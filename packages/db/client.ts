1e198a27ebbd4a0c0a9763349b7564ea4a9e03ae
LeonAlvarez <leon@leon-design.es>
2024-10-02 12:35:02 +0200

    drizzle migrations

---
+++ packages/db/client.ts
@@ -0,0 +1,19 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import pg from 'pg'
+
+import { users, posts } from "./schema";
+import { poolConnectionString } from "./drizzle.config";
+
+const { PG_MAX_CLIENTS } = process.env;
+
+export const queryClient = new pg.Pool({
+  connectionString: poolConnectionString,
+  ssl: process.env.PG_SSL_REQUIRE === 'true' ? true : undefined,
+  max: PG_MAX_CLIENTS || 10,
+});
+
+export const db = drizzle(queryClient, {
+  schema: { ...users, ...posts },
+});
+
+export default db;
--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from "drizzle-kit";
 
 export const migrationsFolder = "./migrations";
 
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB,PG_BOUNCER_PORT } = process.env;
+
+export const connectionString = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable`;
+export const poolConnectionString = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${PG_BOUNCER_PORT}/${POSTGRES_DB}?sslmode=disable`;
+
 export default defineConfig({
   dialect: "postgresql",
   schema: "./schema/*",
   out: migrationsFolder,
   dbCredentials: {
-    url: process.env.DATABASE_URL as string,
+    url: connectionString,
     ...(process.env.DATABASE_DRIVER && { driver: process.env.DATABASE_DRIVER }),
   },
   verbose: true,
