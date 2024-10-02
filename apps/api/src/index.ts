4a1bcbbedad5d07986ebcfbdb82797545910a237
LeonAlvarez <leon@leon-design.es>
2024-10-02 13:00:02 +0200

    Basic api endpoints for users/post

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,19 @@
+import 'dotenv/config'
+
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-
+import {db, schema} from 'db';
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/users', async(c) => {
+  const users = await db.select().from(schema.users);
+  return c.json(users)
+});
+
+app.get('/posts', async(c) => {
+  const posts = await db.select().from(schema.posts);
+  return c.json(posts)
+});
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
@@ -13,4 +21,4 @@ console.log(`Server is running on port ${port}`)
 serve({
   fetch: app.fetch,
   port
-})
+});
---
+++ packages/db/index.ts
@@ -0,0 +1,3 @@
+export { db } from './client';
+
+export * as schema from './schema';
